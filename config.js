5ba21527d6ff4c24299f6652ec5561f28954d3b7
Jarrett Cruger <jcrugzz@gmail.com>
2016-11-30 10:05:51 -0500

    [dist] config

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  config: {
+    keyspace: 'bffs',
+    user: 'cassandra',
+    password: 'cassandra',
+    hosts: ['127.0.0.1'],
+    keyspaceOptions: {
+      class: 'SimpleStrategy',
+      replication_factor: 1
+    }
+
+  }
+};
